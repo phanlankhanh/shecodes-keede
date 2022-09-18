e8488419887aa583dff2dfa94ca5a19f216a0dbd
Phan Khanh <phanlankhanh2102@gmail.com>
2022-09-18 09:51:23 +0700

    color conflict solved

--- lib/style/color.dart
+++ lib/style/color.dart
@@ -4,8 +4,6 @@ class AppColors {
   static const white = Colors.white;
   static const purple = Color(0xffB8B1FB);
   static const darkPurple = Color(0xff6A62B7);
-  static const mainPurple = Color(0xff6a62b6);
-  static const lightPurple = Color(0xffa6a6a6);
 
   static const iconGray = Color(0xff000000);
   static const primaryGreen = Color(0xff014519);
