e95669ff6d2e229db4204358dcb50b10f6e63a51
Thanh Ngoc <tngocnp@pm.me>
2022-09-20 15:47:17 +0700

    change navigator link & remove unused import

---
+++ lib/createPlan.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewPlan extends StatelessWidget {
+  const NewPlan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travel_blog/home.dart';
+import 'package:travel_blog/createPlan.dart';
 import 'package:travel_blog/style/color.dart';
 
 class Login extends StatelessWidget {
@@ -24,7 +24,7 @@ class Login extends StatelessWidget {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => HomePage()));
+                                  builder: (context) => NewPlan()));
                         },
                         child: Text(
                           'Skip',
